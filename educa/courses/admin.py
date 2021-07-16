c5beb1d674215434691dcf6c40ed18e3279cb967
hussamEL-Hwary <hossam.elhwary@stud.fci-cu.edu.eg>
2021-07-16 15:45:35 +0200

    upate .gitignore

--- educa/courses/admin.py
+++ educa/courses/admin.py
@@ -14,5 +14,5 @@ class CourseAdmin(admin.ModelAdmin):
     list_display = ['title', 'subject', 'created']
     list_filter = ['created', 'subject']
     search_fields = ['title', 'overview']
-    prepopulated_fields = ['slug', 'title']
+    prepopulated_fields = {'slug': ('title',)}
     inlines = [ModuleInline]
