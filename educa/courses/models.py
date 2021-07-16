bf79ebab89dece32745dffdb4772b3347f2558b6
hussamEL-Hwary <hossam.elhwary@stud.fci-cu.edu.eg>
2021-07-16 15:47:16 +0200

    add items model

--- educa/courses/models.py
+++ educa/courses/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
+from .fields import OrderField
 
 class Subject(models.Model):
     title = models.CharField(max_length=200)
@@ -34,6 +37,49 @@ class Module(models.Model):
                                 on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     description = models.TextField(blank=True)
+    order = OrderField(blank=True, for_fields=['courses'])
+
+    class Mets:
+        ordering = ['order']
 
     def __str__(self):
+        return f'{self.order}. {self.title}'
+
+
+class Content(models.Model):
+    module = models.ForeignKey(Module, 
+                               related_name='content', 
+                               on_delete=models.CASCADE,
+                               limit_choices_to={'model__in':(
+                                   'text',
+                                   'video',
+                                   'image',
+                                   'file')})
+    content_type = models.ForeignKey(ContentType, 
+                                     on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    item = GenericForeignKey('content_type', 'object_id')
+    order = OrderField(blank=True, for_fields=['courses'])
+
+
+class ItemBase(models.Model):
+    owner = models.ForeignKey(User, 
+                              related_name='%(class)s_related',
+                              on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        abstract = True
+    def __str__(self):
         return self.title
+    
+class Text(ItemBase):
+    content = models.TextField()
+
+class File(ItemBase):
+    file = models.FileField(upload_to='files')
+
+class Video(ItemBase):
+    url = models.URLField()
