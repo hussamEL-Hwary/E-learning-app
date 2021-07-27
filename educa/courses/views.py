7be0d2ba5c5efb6fddfa044d3178b17c0ad82d76
hussamEL-Hwary <hossam.elhwary@stud.fci-cu.edu.eg>
2021-07-27 17:21:31 +0200

    add content order view

--- educa/courses/views.py
+++ educa/courses/views.py
@@ -137,7 +137,7 @@ class ModuleContentListView(TemplateResponseMixin, View):
     def get(self, request, module_id):
         module = get_object_or_404(Module,
                                    id=module_id,
-                                   course__owner=request.user)
+                                   courses__owner=request.user)
         return self.render_to_response({'module': module})
 
 class ContentDeleteView(View):
@@ -157,6 +157,16 @@ class ModuleOrderView(CsrfExemptMixin,
     def post(self, request):
         for id, order in self.request_json.items():
             Module.objects.filter(id=id, 
-                                  course__owner=request.user).update(order=order)
+                                  courses__owner=request.user).update(order=order)
         return self.render_json_response({'success': 'OK'})
+
+
+class ContentOrderView(CsrfExemptMixin,
+                       JsonRequestResponseMixin,
+                       View):
+    def post(self, request):
+        for id, order in self.request_json.items():
+            Content.objects.filter(id=id,
+                                   module__courses__owner=request.user).update(order=order)
+        return self.render_json_response({'saved': 'OK'})
                     
